56f36de5939c9bb64f397b9e2407d3cb1044a0cc
arthursl12 <arthursoutolima@gmail.com>
2020-03-29 14:26:25 -0300

    Mais design e página de busca - Design da página principal (que virou página de busca) - Implementação inicial do método de busca (necessita adaptação e integração com o BD)

--- books/app/forms.py
+++ books/app/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+class BookSearchForm(FlaskForm):
+    choices = [('title','Book Title'),
+               ('author','Author'),
+               ('isbn','ISBN')]
+    select = SelectField('Search for books:', choices=choices)
+    search = StringField('')
--- books/app/routes.py
+++ books/app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template,session, flash, redirect, url_for
+from flask import render_template,session, flash, redirect, url_for, request
+
 
 from app import app
 from app import db
-from app.forms import LoginForm
+from app.forms import LoginForm, BookSearchForm
 from app.users import checkPassword, findUser
 
 
@@ -11,10 +12,15 @@ from app.users import checkPassword, findUser
 def index():
     return redirect(url_for('login'))
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     user = findUser(session['user_id'])
-    return render_template('search.html', title='Home', user=user, userlogged=True)
+    search = BookSearchForm()
+    if request.method == 'POST':
+        return search_results(search)
+    return render_template('search.html', 
+        title='Home', user=user, userlogged=True,
+        form=search)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -38,3 +44,19 @@ def login():
 def logout():
     session.clear()
     return redirect(url_for('login'))
+
+@app.route('/results')
+def search_results(search):
+    # TODO: adaptar essa função para o SQLAlchemy puro
+    # Função original de http://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/
+    results = []
+    search_string = search.data['search']
+    if search.data['search'] == '':
+        qry = db_session.query(Album)
+        results = qry.all()
+    if not results:
+        flash('No results found!')
+        return redirect('/')
+    else:
+        # display results
+        return render_template('results.html', results=results)
