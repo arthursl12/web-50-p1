87ae55efefd586c6fabfd79a30b22a1163e9ed5d
arthursl12 <arthursoutolima@gmail.com>
2020-03-29 07:25:12 -0300

    This version works - Lets hope so

---
+++ books/app/__init__.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from app.config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Session(app)
+engine = create_engine(os.getenv("DATABASE_URL")) # database engine object from SQLAlchemy that manages connections to the database
+                                                    # DATABASE_URL is an environment variable that indicates where the database lives
+db = scoped_session(sessionmaker(bind=engine))
+
+
+
+
+
+from app import routes
+
+app.run(debug=True)
